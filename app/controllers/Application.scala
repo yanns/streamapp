4019cbcd9df1e7ef118e2091f210197ea7168aeb
Yann Simon <yann.simon@epost-dev.de>
2013-08-13 09:59:07 +0200

    send chunks with iteratee

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,11 @@
 package controllers
 
 import play.api.mvc._
-import play.api.libs.iteratee.{Concurrent, Enumerator}
+import play.api.libs.iteratee._
+import scala.concurrent.{Future, Promise}
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.mvc.ResponseHeader
+import play.api.mvc.SimpleResult
 
 object Application extends Controller {
   
@@ -34,4 +38,75 @@ object Application extends Controller {
 
   }
   
+  def streamFromIteratee = Action { request =>
+
+    val content = "hello world ".*(10).getBytes
+
+    val (iteratee, channel) = joined[Array[Byte]]
+
+    // send first chunk
+    Enumerator(content)(iteratee) map { it =>
+      // send second chunk
+      Enumerator(content)(it) map { it2 =>
+        // finish streaming
+        Enumerator.eof(it2)
+      }
+    }
+
+    SimpleResult(
+      header = ResponseHeader(
+        OK,
+        Map(
+          CONTENT_LENGTH.toString -> (content.size * 2).toString,
+          CONTENT_DISPOSITION -> s"""attachment; filename="stream2-test.txt"""",
+          CONTENT_TYPE -> "plain/txt"
+        )),
+      body = channel
+    )
+
+  }
+
+  /**
+   * Create a joined iteratee enumerator pair.
+   *
+   * When the enumerator is applied to an iteratee, the iteratee subsequently consumes whatever the iteratee in the pair
+   * is applied to.  Consequently the enumerator is "one shot", applying it to subsequent iteratees will throw an
+   * exception.
+   */
+  private def joined[A]: (Iteratee[A, Unit], Enumerator[A]) = {
+    val promisedIteratee = Promise[Iteratee[A, Unit]]()
+    val enumerator = new Enumerator[A] {
+      def apply[B](i: Iteratee[A, B]): Future[Iteratee[A, B]] = {
+        val doneIteratee = Promise[Iteratee[A, B]]()
+
+        // Equivalent to map, but allows us to handle failures
+        def wrap(delegate: Iteratee[A, B]): Iteratee[A, B] = new Iteratee[A, B] {
+          def fold[C](folder: (Step[A, B]) => Future[C]) = {
+            val toReturn = delegate.fold {
+              case done @ Step.Done(a, in) => {
+                doneIteratee.success(done.it)
+                folder(done)
+              }
+              case Step.Cont(k) => {
+                folder(Step.Cont(k.andThen(wrap)))
+              }
+              case err => folder(err)
+            }
+            toReturn.onFailure {
+              case e => doneIteratee.failure(e)
+            }
+            toReturn
+          }
+        }
+
+        if (promisedIteratee.trySuccess(wrap(i).map(_ => ()))) {
+          doneIteratee.future
+        } else {
+          throw new IllegalStateException("Joined enumerator may only be applied once")
+        }
+      }
+    }
+    (Iteratee.flatten(promisedIteratee.future), enumerator)
+  }
+
 }

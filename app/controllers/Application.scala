3884f1e5aab74c0b8cfabf022ed4de9b6f44a03b
Yann Simon <yann.simon.fr@gmail.com>
2014-02-15 10:45:38 +0100

    use joined from play

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,11 @@ package controllers
 
 import play.api.mvc._
 import play.api.libs.iteratee._
-import scala.concurrent.{ ExecutionContext, Future, Promise }
+import scala.concurrent.Promise
 import play.api.mvc.ResponseHeader
 import play.api.mvc.SimpleResult
 import play.api.libs.ws.{ResponseHeaders, WS}
+import play.api.libs.iteratee.Concurrent.joined
 
 object Application extends Controller {
   
@@ -97,52 +98,6 @@ object Application extends Controller {
     WS.url("http://downloads.typesafe.com/play/2.1.3/play-2.1.3.zip").get(consumer).map(_.run)
 
     resultPromise.future
-  }
-
-
-  /**
-   * Create a joined iteratee enumerator pair.
-   *
-   * When the enumerator is applied to an iteratee, the iteratee subsequently consumes whatever the iteratee in the pair
-   * is applied to.  Consequently the enumerator is "one shot", applying it to subsequent iteratees will throw an
-   * exception.
-   */
-  private def joined[A]: (Iteratee[A, Unit], Enumerator[A]) = {
-    val promisedIteratee = Promise[Iteratee[A, Unit]]()
-    val enumerator = new Enumerator[A] {
-      def apply[B](i: Iteratee[A, B]): Future[Iteratee[A, B]] = {
-        val doneIteratee = Promise[Iteratee[A, B]]()
-
-        // Equivalent to map, but allows us to handle failures
-        def wrap(delegate: Iteratee[A, B]): Iteratee[A, B] = new Iteratee[A, B] {
-          def fold[C](folder: (Step[A, B]) => Future[C])(implicit ec: ExecutionContext): Future[C] = {
-            val toReturn = delegate.fold {
-              case done @ Step.Done(a, in) => {
-                doneIteratee.success(done.it)
-                folder(done)
-              }
-              case Step.Cont(k) => {
-                folder(Step.Cont(k.andThen(wrap)))
-              }
-              case err => folder(err)
-            }
-            toReturn.onFailure {
-              case e => doneIteratee.failure(e)
-            }
-            toReturn
-          }
-        }
-
-        import play.api.libs.concurrent.Execution.Implicits._
-
-        if (promisedIteratee.trySuccess(wrap(i).map(_ => ()))) {
-          doneIteratee.future
-        } else {
-          throw new IllegalStateException("Joined enumerator may only be applied once")
-        }
-      }
-    }
-    (Iteratee.flatten(promisedIteratee.future), enumerator)
   }
 
 }
